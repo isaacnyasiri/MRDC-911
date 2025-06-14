847268335ffd67de1e098472413a4190d63ae6ae
Isaac Nyasiri Omung'ala <inyasiri@riarauniversity.ac.ke>
2025-06-14 17:43:25 +0300

    Delete Q4.R

--- Q4.R
+++
@@ -1,9 +0,0 @@
-ggplot(data, aes(x = study_hours_weekly)) +
-  geom_histogram(binwidth = 2, fill = "#443785", color = "white") +
-  facet_wrap(~residency) +
-  labs(
-    title = "Study Hours by Residency",
-    x = "Weekly Study Hours",
-    y = "Number of Students"
-  ) +
-  theme_minimal()
