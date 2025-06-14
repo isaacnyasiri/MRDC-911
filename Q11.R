42b86210f9a8fe4a15463a223138170443a5b814
Isaac Nyasiri Omung'ala <inyasiri@riarauniversity.ac.ke>
2025-06-14 17:41:40 +0300

    Delete Q11.R

--- Q11.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-hist_before <- ggplot(data, aes(x = attendance_rate)) +
-  geom_histogram() +
-  ggtitle("Before Imputation")
-data <- data %>%
-  mutate(attendance_rate = ifelse(is.na(attendance_rate), mean(attendance_rate, na.rm = TRUE), attendance_rate))
-hist_after <- ggplot(data, aes(x = attendance_rate)) +
-  geom_histogram() +
-  ggtitle("After Imputation")
