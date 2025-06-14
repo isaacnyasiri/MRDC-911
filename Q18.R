0d59bc04cdca01b4d9df10f508585c2a6eeb401a
Isaac Nyasiri Omung'ala <inyasiri@riarauniversity.ac.ke>
2025-06-14 17:42:56 +0300

    Delete Q18.R

--- Q18.R
+++
@@ -1,9 +0,0 @@
-ggplot(data, aes(x = study_hours_weekly, y = total_score)) +
-geom_point(alpha = 0.6, color = "steelblue") +
-facet_wrap(~ residency) +
-labs(
-title = "Study Hours vs Total Score by Residency",
-   x = "Weekly Study Hours",
-    y = "Total Score"
-  ) +
-  theme_light()
